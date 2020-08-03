eaaaf20bb34e48563dfc6aa98b8263b50479510e
pietdevries94 <piet@devries.tech>
2020-08-03 20:09:48 +0200

    Add vue template

---
+++ templates/vue/shell.nix
@@ -0,0 +1,14 @@
+{ sources ? import ./nix/sources.nix}:
+
+let
+  niv = (import sources.niv {}).niv;
+  pkgs = import sources.nixpkgs {};
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    niv
+
+    yarn
+    nodejs-12_x
+    nodePackages."@vue/cli"
+  ];
+}
